0d8b1ac4da9eba6fb4bfa87c58468aedb0c6a90d
Shaqeal Jinnah <j.shaqeal@gmail.com>
2023-09-11 23:41:22 +1000

    completed Modal

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,21 +1,37 @@
-import { Movie } from "@/typings"
-import Image from "next/image"
+import { modalState, movieState } from "@/atoms/modalAtom";
+import { Movie } from "@/typings";
+import Image from "next/image";
+import { useRecoilState } from "recoil";
 
 interface Props {
-    movie: Movie 
-    // movie: Movie | DocumentData
+  movie: Movie;
+  // movie: Movie | DocumentData
 }
+
 
 function Thumbnail({ movie }: Props) {
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+
   return (
-    <div className="relative h-20 min-w-[180px] cursor-pointer transition duration-200 ease-out
-    md:h-36 md:min-w-[260px] md:hover:scale-105">
-        <Image src={`https://image.tmdb.org/t/p/w500${
+    <div
+      className="relative h-20 min-w-[180px] cursor-pointer transition duration-200 ease-out
+    md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={() => {
+        setCurrentMovie(movie);
+        setShowModal(true);
+      }}
+    >
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
-        }`} alt="" layout="fill" className="rounded-sm object-cover md:rounded"/>
+        }`}
+        alt=""
+        layout="fill"
+        className="rounded-sm object-cover md:rounded"
+      />
     </div>
-  )
+  );
 }
 
-
-export default Thumbnail
+export default Thumbnail;
