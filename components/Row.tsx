3320ca382d0987318de2fa6cf77b7547feeb4d7f
Shaqeal Jinnah <j.shaqeal@gmail.com>
2023-10-10 14:55:26 +1100

    fixed a bug

--- components/Row.tsx
+++ components/Row.tsx
@@ -6,7 +6,7 @@ import { DocumentData } from "firebase/firestore";
 
 interface Props {
   title: string;
-  movies: Movie | DocumentData[]
+  movies: Movie[] | DocumentData[]
 }
 
 function Row({ title, movies }: Props) {
